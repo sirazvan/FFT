efebda56d3feb119d8b5e2d693f8cfd17fb5a523
sirazvan <141137940+sirazvan@users.noreply.github.com>
2025-03-28 11:48:03 +0200

    Update butterfly_type4.sv

--- butterfly_type4.sv
+++ butterfly_type4.sv
@@ -7,15 +7,21 @@ module butterfly_type4 #(
     input  logic en,
     input  logic rst_n, 
 
-    input  logic signed [DATA_WIDTH-1:0] real_in0, imag_in0,
-    input  logic signed [DATA_WIDTH-1:0] real_in1, imag_in1,
-    output logic signed [DATA_WIDTH-1:0] real_out0, imag_out0,
-    output logic signed [DATA_WIDTH-1:0] real_out1, imag_out1
+    input  logic signed [DATA_WIDTH-1:0] real_in0, 
+    input  logic signed [DATA_WIDTH-1:0] imag_in0,
+    input  logic signed [DATA_WIDTH-1:0] real_in1, 
+    input  logic signed [DATA_WIDTH-1:0] imag_in1,
+    output logic signed [DATA_WIDTH-1:0] real_out0, 
+    output logic signed [DATA_WIDTH-1:0] imag_out0,
+    output logic signed [DATA_WIDTH-1:0] real_out1, 
+    output logic signed [DATA_WIDTH-1:0] imag_out1
 );
 
     logic signed [DATA_WIDTH-1:0] sum, diff;
-    logic signed [2*DATA_WIDTH-1:0] mult_real, mult_imag;
-    logic signed [DATA_WIDTH-1:0] twiddle_mult_real, twiddle_mult_imag;
+    logic signed [2*DATA_WIDTH-1:0] mult_real;
+    logic signed [2*DATA_WIDTH-1:0] mult_imag;
+    logic signed [DATA_WIDTH-1:0] twiddle_mult_real;
+    logic signed [DATA_WIDTH-1:0] twiddle_mult_imag;
     
     always_ff @(posedge clk) begin
         mult_real <= (real_in1 * TWIDDLE_REAL) - (imag_in1 * TWIDDLE_IMAG);
