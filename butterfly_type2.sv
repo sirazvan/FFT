5e8a0a2e9cb595f4be792753355f2af845caad04
sirazvan <141137940+sirazvan@users.noreply.github.com>
2025-04-02 17:57:14 +0300

    Update butterfly_type2.sv

--- butterfly_type2.sv
+++ butterfly_type2.sv
@@ -1,23 +1,24 @@
 module butterfly_type2 #(
-    parameter DATA_WIDTH = 16
+    parameter DATA_WIDTH = 16,
+    parameter VIRTUAL_DATA_WIDTH = 18
 )(
     input  logic clk,
-    input  logic en,
+    input  logic enable,
     input  logic rst_n, 
 
-    input  logic signed [DATA_WIDTH-1:0] real_in0,
-    input  logic signed [DATA_WIDTH-1:0] imag_in0,
-    input  logic signed [DATA_WIDTH-1:0] real_in1, 
-    input  logic signed [DATA_WIDTH-1:0] imag_in1,
-    output logic signed [DATA_WIDTH-1:0] real_out0,
-    output logic signed [DATA_WIDTH-1:0] imag_out0,
-    output logic signed [DATA_WIDTH-1:0] real_out1,
-    output logic signed [DATA_WIDTH-1:0] imag_out1
+    input  logic signed [VIRTUAL_DATA_WIDTH-1:0] real_in0,
+    input  logic signed [VIRTUAL_DATA_WIDTH-1:0] imag_in0,
+    input  logic signed [VIRTUAL_DATA_WIDTH-1:0] real_in1, 
+    input  logic signed [VIRTUAL_DATA_WIDTH-1:0] imag_in1,
+    
+    output logic signed [VIRTUAL_DATA_WIDTH-1:0] real_out0,
+    output logic signed [VIRTUAL_DATA_WIDTH-1:0] imag_out0,
+    output logic signed [VIRTUAL_DATA_WIDTH-1:0] real_out1,
+    output logic signed [VIRTUAL_DATA_WIDTH-1:0] imag_out1
 );
 
-    // Pipeline registers
-    logic signed [DATA_WIDTH-1:0] real_temp0, imag_temp0;
-    logic signed [DATA_WIDTH-1:0] real_temp1, imag_temp1;
+    logic signed [VIRTUAL_DATA_WIDTH-1:0] real_temp0, imag_temp0;
+    logic signed [VIRTUAL_DATA_WIDTH-1:0] real_temp1, imag_temp1;
 
     always_ff @(posedge clk) begin
         real_temp0 <= real_in0 - imag_in1;
