ec8d998654d6da00eecffba5c09f671dc41e3458
sirazvan <141137940+sirazvan@users.noreply.github.com>
2025-04-02 17:57:38 +0300

    Update butterfly_type3.sv

--- butterfly_type3.sv
+++ butterfly_type3.sv
@@ -1,24 +1,26 @@
 module butterfly_type3 #(
     parameter DATA_WIDTH = 16,
+    parameter VIRTUAL_DATA_WIDTH = 18,
+    parameter SHIFT_PARAM = 15,
     parameter INVERT_MODE = 0  
 )(
     input  logic clk,
-    input  logic en,
+    input  logic enable,
     input  logic rst_n, 
 
-    input  logic signed [DATA_WIDTH-1:0] real_in0, 
-    input  logic signed [DATA_WIDTH-1:0] imag_in0,
-    input  logic signed [DATA_WIDTH-1:0] real_in1,
-    input  logic signed [DATA_WIDTH-1:0] imag_in1,
-    output logic signed [DATA_WIDTH-1:0] real_out0,
-    output logic signed [DATA_WIDTH-1:0] imag_out0,
-    output logic signed [DATA_WIDTH-1:0] real_out1,
-    output logic signed [DATA_WIDTH-1:0] imag_out1
-); +
-    localparam signed [DATA_WIDTH-1:0] SQRT2_HALF = 16'sd23170;
+    input  logic signed [VIRTUAL_DATA_WIDTH-1:0] real_in0, 
+    input  logic signed [VIRTUAL_DATA_WIDTH-1:0] imag_in0,
+    input  logic signed [VIRTUAL_DATA_WIDTH-1:0] real_in1,
+    input  logic signed [VIRTUAL_DATA_WIDTH-1:0] imag_in1,
+    output logic signed [VIRTUAL_DATA_WIDTH-1:0] real_out0,
+    output logic signed [VIRTUAL_DATA_WIDTH-1:0] imag_out0,
+    output logic signed [VIRTUAL_DATA_WIDTH-1:0] real_out1,
+    output logic signed [VIRTUAL_DATA_WIDTH-1:0] imag_out1
+); 
+    localparam signed [VIRTUAL_DATA_WIDTH-1:0] SQRT2_HALF = signed'(23170);
 
-    logic signed [DATA_WIDTH-1:0] sum, diff;
-    logic signed [2*DATA_WIDTH-1:0] mult_sum, mult_diff;
+    logic signed [VIRTUAL_DATA_WIDTH-1:0] sum, diff;
+    logic signed [2*VIRTUAL_DATA_WIDTH-1:0] mult_sum, mult_diff;
 
     always_ff @(posedge clk) begin
         sum  <= real_in1 + imag_in1;
@@ -30,11 +32,11 @@ module butterfly_type3 #(
         mult_diff <= diff * SQRT2_HALF; 
     end
 
-    logic signed [DATA_WIDTH-1:0] twiddle_mult_real, twiddle_mult_imag;
+    logic signed [VIRTUAL_DATA_WIDTH-1:0] twiddle_mult_real, twiddle_mult_imag;
 
     always_ff @(posedge clk) begin
-        twiddle_mult_real <= (mult_sum + (1 << (DATA_WIDTH - 1))) >>> DATA_WIDTH;  
-        twiddle_mult_imag <= (mult_diff + (1 << (DATA_WIDTH - 1))) >>> DATA_WIDTH; 
+        twiddle_mult_real <= (mult_sum  + (1 << (SHIFT_PARAM - 1))) >>> SHIFT_PARAM;  
+        twiddle_mult_imag <= (mult_diff + (1 << (SHIFT_PARAM - 1))) >>> SHIFT_PARAM; 
     end
     
     generate 
